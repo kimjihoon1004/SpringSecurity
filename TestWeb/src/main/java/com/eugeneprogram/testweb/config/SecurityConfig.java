bc69ca18959204957ea66dfeb3d681a595a6334a
kimjihoon1004 <jihoon4545@gmail.com>
2024-07-16 18:54:06 +0900

    2024-07-16 SpringSecurity permitAll() 적용

--- TestWeb/src/main/java/com/eugeneprogram/testweb/config/SecurityConfig.java
+++ TestWeb/src/main/java/com/eugeneprogram/testweb/config/SecurityConfig.java
@@ -7,10 +7,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.eugeneprogram.testweb.security.CustomAuthenticationProvider;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -29,9 +34,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
         http
             .authorizeHttpRequests((authz) -> authz
-                    .requestMatchers(new AntPathRequestMatcher("/"), new AntPathRequestMatcher("/visa.do")).permitAll()
-                    .anyRequest().permitAll())
+                    .requestMatchers(
+                            new AntPathRequestMatcher("/"),
+                            new AntPathRequestMatcher("/selectDB.do"),
+                            new AntPathRequestMatcher("/testPage"),
+                            new AntPathRequestMatcher("/test.html"),
+                            new AntPathRequestMatcher("/visa.do")
+                    ).permitAll()
+                    .anyRequest().authenticated())
             .httpBasic(withDefaults());
         return http.build();
     }
+    
+    
 }
--- TestWeb/src/main/java/com/eugeneprogram/testweb/controller/MainController.java
+++ TestWeb/src/main/java/com/eugeneprogram/testweb/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.eugeneprogram.testweb.controller;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.HashMap;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 /*import org.apache.catalina.startup.FailedContext;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,12 +17,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.stereotype.Controller;*/
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import com.eugeneprogram.testweb.service.TestService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class MainController {
@@ -32,35 +37,7 @@ public class MainController {
         return "home";
     }
     
-    @GetMapping(value = "/username")
-    @ResponseBody
-    public ModelAndView currentUserName(Principal principal)  {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("username");
-        
-        String userNameString = principal.getName();
-        
-        mv.addObject("userNameString", userNameString);
-        return mv;
-    }
-
-    @RequestMapping(value = "/main.do") //주소명
-    public ModelAndView goMain(@RequestParam("userId") String userId, @RequestParam("userPw") String userPw, Principal principal) {
-        //
-        System.out.println("main.do");
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("main"); //jsp파일 경로
-        
-        mv.addObject("userId", userId);
-        mv.addObject("userPw", userPw);
-        
-        String userNameString = principal.getName();
-        mv.addObject("usameString", userNameString);
-        
-        return mv;
-    }
-
-    @RequestMapping(value = "/dbTest.do")
+    @GetMapping(value = "/dbTest.do")
     public ModelAndView dbTest() throws Exception {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("testDbList");
@@ -80,52 +57,16 @@ public class MainController {
         return mv;
     }
     
-    @RequestMapping(value = "/visa.do")
-    public ModelAndView userLogin(Principal principal) throws Exception  {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("employee/signup");
-        
-        String userNameString = principal.getName();
-        mv.addObject("userNameString", userNameString);
-        
-        return mv;
+    
+    @GetMapping(value = "/selectDB.do", produces = "application/json")
+    public List<Map<String, Object>> selectDB() throws Exception{        
+        return testService.getAllList();
     }
     
-    //@RequestMapping 사용 시 'method = RequestMethod.POST'를 이용하여 visa.do에서 넘어오는 값들을 POST방식(=url에 파라미터가 보이지 않음)으로 넘어올 수 있다.
-    @RequestMapping(value = "/insertUser.do", method = RequestMethod.POST)
-    public String method(String userId, String userName,
-            String userPw, String userPhone, String userDate, Principal principal) throws Exception {        
-        if(principal.equals(null) || principal == null) {
-            System.out.println("---------principal is null-------");            
-        }
-        else {
-            System.out.println("---------" + principal + "---------");
-        }
-        System.out.println(userName + "\n" + userId + "\n" + userPw + "\n" + userPhone + "\n" + userDate);
-        Map<String, Object> userValue = new HashMap<>();        
-        userValue.put("userName", userName);
-        userValue.put("userId", userId);
-        userValue.put("userPw", userPw);
-        userValue.put("userPhone", userPhone);
-        userValue.put("userDate", userDate);
-        int success = testService.insertId(userValue);
-        if(success == 0) {
-            return fail();
-        }
-        else {
-            return success();
-        }             
-        
+    @GetMapping("/testPage")
+    public void getTestPage(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/test.html");
     }
     
-    @GetMapping(value = "/success")
-    public String success() {
-        return "success";
-    }
-    
-    @GetMapping(value = "/fail")
-    public String fail()    {
-        return "fail";
-    }
     
 }
